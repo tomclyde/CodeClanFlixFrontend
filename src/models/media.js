7cf281168d046be4935da3e53281ed198a0d467a
Stephen Rafferty <srafferty73@gmail.com>
2019-02-14 13:55:20 +0000

    Completed get method in model.

--- src/models/media.js
+++ src/models/media.js
@@ -4,16 +4,18 @@ const RequestHelper = require ('../helpers/request_helper.js');
 const Media = function (url) {
   this.url = url;
   this.request = new RequestHelper(this.url);
-}
-
-Media.prototype.bindEvents = function () {
-
 };
+
+// Media.prototype.bindEvents = function () {
+//
+// };
 
 Media.prototype.getData =  function () {
   this.request.get()
     .then((items) => {
+      console.log(items);
       PubSub.publish('Media:data-ready', items);
+      console.log(items);
     })
     .catch(console.error);
 };
