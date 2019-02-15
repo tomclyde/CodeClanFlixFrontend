19c655f9f0e0252dfe96b7d0b32f23de36f0ce66
user <user@users-MacBook-Pro.local>
2019-02-15 22:36:02 +0000

    Did some slight refactoring of the code in the Media View to put some of the elements into a new div to allow for better css.

--- src/views/media_view.js
+++ src/views/media_view.js
@@ -15,20 +15,27 @@ MediaView.prototype.render = function (item) {
   itemName.textContent = item.name;
   itemContainer.appendChild(itemName);
 
+  const innerContainer = document.createElement('div');
+  innerContainer.id = "inner-container";
+  itemContainer.appendChild(innerContainer);
+
+
+
+
+
+
   const itemGenre = document.createElement('p');
   itemGenre.textContent = item.genre;
-  itemContainer.appendChild(itemGenre);
+  itemGenre.id = "item-genre";
+  innerContainer.appendChild(itemGenre);
 
   const itemLanguage = document.createElement('p');
   itemLanguage.textContent = item.language;
-  itemContainer.appendChild(itemLanguage);
-
-  // const itemImage = document.createElement('img');
-  // itemImage.classList.add("movie-image");
-  // itemImage.src = item.image;
-  // itemContainer.appendChild(itemImage);
+  itemLanguage.id = "item-language";
+  innerContainer.appendChild(itemLanguage);
 
   const itemLike = document.createElement("input");
+  itemLike.id = "item-like";
   itemLike.classList.add("toggle-input");
   itemLike.type = "button";
   itemLike.value = item.like;
@@ -38,7 +45,7 @@ MediaView.prototype.render = function (item) {
   else {
     itemLike.value = "Like"
   };
-  itemContainer.appendChild(itemLike);
+  innerContainer.appendChild(itemLike);
 
   itemLike.addEventListener('click', (event) => {
     console.log("value of liked film", itemLike.value);
@@ -54,6 +61,69 @@ MediaView.prototype.render = function (item) {
 
   return itemLike;
   });
+
+
+
+
+
+
+  // itemContainer.addEventListener('mouseover', () => {
+  //   const itemGenre = document.createElement('p');
+  //   itemGenre.textContent = item.genre;
+  //   itemGenre.id = "item-genre";
+  //   innerContainer.appendChild(itemGenre);
+  //
+  //   const itemLanguage = document.createElement('p');
+  //   itemLanguage.textContent = item.language;
+  //   itemLanguage.id = "item-language";
+  //   innerContainer.appendChild(itemLanguage);
+  //
+  //   const itemLike = document.createElement("input");
+  //   itemLike.id = "item-like";
+  //   itemLike.classList.add("toggle-input");
+  //   itemLike.type = "button";
+  //   itemLike.value = item.like;
+  //   if (item.like === false){
+  //     itemLike.value = "Don't Like"
+  //   }
+  //   else {
+  //     itemLike.value = "Like"
+  //   };
+  //   innerContainer.appendChild(itemLike);
+  //
+  //   itemLike.addEventListener('click', (event) => {
+  //     console.log("value of liked film", itemLike.value);
+  //     if (itemLike.value === "Don't Like"){
+  //       item.like = true;
+  //     }else{
+  //       item.like = false;
+  //     }
+  //     const dropdown = document.querySelector('select');
+  //     const jointKey = {id: `${item._id}`, like: item.like, genre: dropdown.value};
+  //     console.log('jointkey', jointKey);
+  //     PubSub.publish('MediaView:toggleButton-clicked', jointKey);
+  //
+  //   return itemLike;
+  //   });
+  // });
+  //
+  // itemContainer.addEventListener('mouseout', () => {
+  //   const itemGenre = document.querySelector('#item-genre');
+  //   const itemLanguage = document.querySelector('#item-language');
+  //   const itemLike = document.querySelector('#item-like');
+  //   innerContainer.removeChild(itemGenre);
+  //   innerContainer.removeChild(itemLanguage);
+  //   innerContainer.removeChild(itemLike);
+  // });
+
+
+
+  // const itemImage = document.createElement('img');
+  // itemImage.classList.add("movie-image");
+  // itemImage.src = item.image;
+  // itemContainer.appendChild(itemImage);
+
+
 
   this.container.appendChild(itemContainer);
 }
